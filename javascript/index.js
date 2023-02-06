9bc21d184a71e87c781a223fa0923655472095ff
Patricio Sanchez Vuichard <patriciosanchezvuichard@gmail.com>
2023-02-06 18:55:21 +0100

    lab terminado junto con los bonus

--- javascript/index.js
+++ javascript/index.js
@@ -39,9 +39,8 @@ getInstruction('mashedPotatoes', 0, (step1) => {
         document.querySelector("#mashedPotatoes").innerHTML += `<li>${step4}</li>`
         getInstruction('mashedPotatoes', 4, (step5) => {
           document.querySelector("#mashedPotatoes").innerHTML += `<li>${step5}</li>`
-          // getInstruction = () => {
-            document.querySelector("#mashedPotatoes").innerHTML += `<li>Mashed potatoes are ready!</li>`
-          // }
+          document.querySelector("#mashedPotatoes").innerHTML += `<li>Mashed potatoes are ready!</li>`
+          document.getElementById("mashedPotatoesImg").removeAttribute("hidden");
         }, (error) => {
           console.log(error)
         })
@@ -92,17 +91,73 @@ obtainInstruction('steak', 0)
   })
   .then( (step7) => {
     document.querySelector("#steak").innerHTML += `<li>${step7}</li>`
+    document.querySelector("#steak").innerHTML += `<li>Stake is ready!</li>`
+    document.getElementById("steakImg").removeAttribute("hidden");
     return obtainInstruction('steak', 8)
   })
   .catch((error) => {
       console.log(error)
   })
-  // ... Your code here
 
 
 
 // Iteration 3 using async/await
-// ...
+async function makeBroccoli() {
+  try {
+    const response1 = await obtainInstruction("broccoli", 0)
+    document.querySelector("#broccoli").innerHTML += `<li>${response1}</li>`
+    // return response1
+  
+    const response2 = await obtainInstruction("broccoli", 1)
+    document.querySelector("#broccoli").innerHTML += `<li>${response2}</li>`
+    // return obtainInstruction("broccoli", 1)
+
+    const response3 = await obtainInstruction("broccoli", 2)
+    document.querySelector("#broccoli").innerHTML += `<li>${response3}</li>`
+  
+    const response4 = await obtainInstruction("broccoli", 3)
+    document.querySelector("#broccoli").innerHTML += `<li>${response4}</li>`
+
+    const response5 = await obtainInstruction("broccoli", 4)
+    document.querySelector("#broccoli").innerHTML += `<li>${response5}</li>`
+
+    const response6 = await obtainInstruction("broccoli", 5)
+    document.querySelector("#broccoli").innerHTML += `<li>${response6}</li>`
+  
+    const response7 = await obtainInstruction("broccoli", 6)
+    document.querySelector("#broccoli").innerHTML += `<li>${response7}</li>`
+    document.querySelector("#broccoli").innerHTML += `<li>Broccoli is ready!</li>`
+    document.getElementById("broccoliImg").removeAttribute("hidden");
+  }
+  catch (error) {
+    console.log(error)
+  }
+}
+makeBroccoli();
 
 // Bonus 2 - Promise all
-// ...
+Promise.all([
+  obtainInstruction("brusselsSprouts", 0),
+  obtainInstruction("brusselsSprouts", 1),
+  obtainInstruction("brusselsSprouts", 2),
+  obtainInstruction("brusselsSprouts", 3),
+  obtainInstruction("brusselsSprouts", 4),
+  obtainInstruction("brusselsSprouts", 5),
+  obtainInstruction("brusselsSprouts", 6),
+  obtainInstruction("brusselsSprouts", 7),
+])
+.then((response) => {
+  document.querySelector("#brusselsSprouts").innerHTML += `<li>${brusselsSprouts[0]}</li>`;
+  document.querySelector("#brusselsSprouts").innerHTML += `<li>${brusselsSprouts[1]}</li>`;
+  document.querySelector("#brusselsSprouts").innerHTML += `<li>${brusselsSprouts[2]}</li>`;
+  document.querySelector("#brusselsSprouts").innerHTML += `<li>${brusselsSprouts[3]}</li>`;
+  document.querySelector("#brusselsSprouts").innerHTML += `<li>${brusselsSprouts[4]}</li>`;
+  document.querySelector("#brusselsSprouts").innerHTML += `<li>${brusselsSprouts[5]}</li>`;
+  document.querySelector("#brusselsSprouts").innerHTML += `<li>${brusselsSprouts[6]}</li>`;
+  document.querySelector("#brusselsSprouts").innerHTML += `<li>${brusselsSprouts[7]}</li>`;
+  document.querySelector("#brusselsSprouts").innerHTML += `<li>Brussels sprouts are ready!</li>`;
+  document.getElementById("brusselsSproutsImg").removeAttribute("hidden");
+})
+.catch((error) => {
+  console.log(error)
+})
